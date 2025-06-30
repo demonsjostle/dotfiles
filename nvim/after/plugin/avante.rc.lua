5db06e764b7b013e20fa028e5add17f094567620
demonsjostle <demons.jostle@gmail.com>
2025-06-30 12:18:10 +0700

    Clean packer setup

--- nvim/after/plugin/avante.rc.lua
+++
@@ -1,8 +0,0 @@
-local status, avante = pcall(require, "avante")
-if (not status) then return end
-
-avante.setup({
-  provider = "openai",
-  endpoint = "https://api.openai.com/v1",
-  model = "gpt-4o-mini",
-})
