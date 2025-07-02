8295f736753ee478e03866551f9d8273555c4213
demonsjostle <demons.jostle@gmail.com>
2025-07-02 22:27:23 +0700

    config bufferline

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  keys = {
+    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+  },
+  opts = {
+    options = {
+      mode = "tabs",
+      -- separator_style = "slant",
+      show_buffer_close_icons = true,
+      show_close_icon = true,
+    },
+  },
+}
