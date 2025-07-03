fc8f135ebfbe19fddf6afea42c50268b12311389
demonsjostle <demons.jostle@gmail.com>
2025-07-03 17:58:03 +0700

    feat(nvim): add lazy extras for (go)

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = { colorscheme = "solarized-osaka" } },
+    -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.go" },
     -- import/override with your plugins
     { import = "plugins" },
   },
