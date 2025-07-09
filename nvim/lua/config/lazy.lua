87aba0452cdad6cb0ef1e2c513d828d75541841b
demonsjostle <demons.jostle@gmail.com>
2025-07-09 13:11:09 +0700

    feat(nvim): add plugin extras for python

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     -- import/override with your plugins
     { import = "plugins" },
   },
