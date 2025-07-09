db29ac5c26db1f3afe56543afe032101e4cdddb8
demonsjostle <demons.jostle@gmail.com>
2025-07-09 11:47:57 +0700

    feat(nvim): add blink.cmp config

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,21 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      ["<Tab>"] = {
+        function(cmp)
+          return cmp.select_next()
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+      ["<S-Tab>"] = {
+        function(cmp)
+          return cmp.select_prev()
+        end,
+        "snippet_backward",
+        "fallback",
+      },
+    },
+  },
+}
