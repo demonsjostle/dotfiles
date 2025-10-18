684556841ffbfc6234ebab2e457348d7b93f8e22
demonsjostle <demons.jostle@gmail.com>
2025-10-19 02:47:12 +0700

    feat(nvim): add plugin extras for rust

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     -- import/override with your plugins
     { import = "plugins" },
   },
